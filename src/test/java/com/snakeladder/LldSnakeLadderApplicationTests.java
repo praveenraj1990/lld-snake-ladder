7ac0319b3ec4cdd89fcf552544d820604ec6a181
praveen-raj <praveen.raj@mettl.com>
2022-01-22 19:15:12 +0530

    Initial commit

---
+++ src/test/java/com/snakeladder/LldSnakeLadderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snakeladder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LldSnakeLadderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
